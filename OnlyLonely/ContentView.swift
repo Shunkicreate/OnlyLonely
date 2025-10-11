2ca34d9e5f288517a34b20e7debf69e0f15e541b
taishin-miyamoto <taishin.miyamoto@dena.com>
2025-10-11 16:03:39 +0900

    プロジェクト作成

---
+++ OnlyLonely/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  OnlyLonely
+//
+//  Created by Taishin Miyamoto on 2025/10/11.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ OnlyLonely/OnlyLonelyApp.swift
@@ -0,0 +1,17 @@
+//
+//  OnlyLonelyApp.swift
+//  OnlyLonely
+//
+//  Created by Taishin Miyamoto on 2025/10/11.
+//
+
+import SwiftUI
+
+@main
+struct OnlyLonelyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
